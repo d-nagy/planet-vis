47d1a5a61d9e2dc2af44d59c5f886a7409f40b2d
Daniel Nagy <danielnagy69@gmail.com>
2021-03-05 18:24:40 +0000

    Add WIP topography extraction script and utilities

---
+++ readTopography.py
@@ -0,0 +1,186 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from utils import *
+
+heightRange = np.array([-8000, 14000])
+
+cmapX = np.array([654, 2296])
+cmapDims = np.array([2682, 97])
+cmapSegmentWidth = 9
+cmapDividerColour = np.full(3, 245)
+
+westHemisphereX = np.array([13, 151])
+eastHemisphereX = np.array([2027, 151])
+hemisphereDims = np.array([1962, 1960])
+
+sf = 8
+
+img = cv2.imread('data/elevationData.tif')
+# img = cv2.imread('data/elevationData_colour_enhanced.tif')
+width = int(img.shape[1] / sf)
+height = int(img.shape[0] / sf)
+smallImg = cv2.resize(img, (width, height), interpolation=cv2.INTER_AREA)
+
+
+#
+# Construct colormap from image
+#
+rawCmap = img[
+    cmapX[1] + cmapDims[1] // 2,
+    cmapX[0]:cmapX[0] + cmapDims[0]
+]
+
+medianCmap = []
+for i in range(0, len(rawCmap), cmapSegmentWidth):
+    segment = np.copy(rawCmap[i:i+cmapSegmentWidth])
+    isCmapDivider = [all(np.greater(segColor, cmapDividerColour)) for segColor in segment]
+    if any(isCmapDivider):
+        centerIndex = np.argmax(segment, axis=0)[0]
+        segment = np.concatenate((segment[:centerIndex-2], segment[centerIndex+3:]))
+    medianCmap.append(np.median(segment, axis=0))
+
+medianCmap = stableUnique(np.array(medianCmap).astype(img.dtype), axis=0)
+medianCmapHsv = cv2.cvtColor(np.array([medianCmap]), cv2.COLOR_BGR2HSV)[0]
+
+cmapBgr = interpColormap(medianCmap, 5)
+cmapHsv = interpColormap(medianCmapHsv, 10, isHsv=True)
+
+
+westHemi = getBoxRegion(img, westHemisphereX, hemisphereDims)
+eastHemi = getBoxRegion(img, eastHemisphereX, hemisphereDims)
+westHemiSmall = getBoxRegion(smallImg, westHemisphereX//sf, hemisphereDims//sf)
+eastHemiSmall = getBoxRegion(smallImg, eastHemisphereX//sf, hemisphereDims//sf)
+
+#
+# Plot RGB cube with colormap embedded as a black line
+#
+# fig = plt.figure(figsize=[10, 10])
+# ax = fig.add_subplot(projection='3d')
+
+# colors = np.column_stack(np.where(np.ones((256, 256, 256))))
+# xs, ys, zs = np.where(np.ones((256, 256, 256)))
+
+# everyNth = 750
+# ax.scatter(xs[::everyNth], ys[::everyNth], zs[::everyNth], s=20, c=colors[::everyNth]/255.0, alpha=0.1)
+# xs, ys, zs = np.flip(cmapBgr, axis=-1).T
+# ax.scatter(xs, ys, zs, c='black')
+# ax.set(xlabel='r', ylabel='g', zlabel='b')
+# plt.show()
+
+
+#
+# Plot HSV cube with colormap embedded as a black line
+#
+# fig = plt.figure(figsize=[10, 10])
+# ax = fig.add_subplot(projection='3d')
+
+# colors = np.column_stack(np.where(np.ones((180, 256, 256)))).astype(img.dtype)
+# colorsRgb = cv2.cvtColor(np.array([colors]), cv2.COLOR_HSV2RGB)[0]
+
+# xs, ys, zs = np.where(np.ones((180, 256, 256)))
+
+# everyNth = 750
+# ax.scatter(xs[::everyNth], ys[::everyNth], zs[::everyNth], s=20, c=colorsRgb[::everyNth]/255.0, alpha=0.1)
+# xs, ys, zs = cmapHsv.T
+# ax.scatter(xs, ys, zs, c=np.arange(xs.size), cmap='plasma', alpha=1)
+# ax.set(xlabel='h', ylabel='s', zlabel='v')
+# plt.show()
+
+
+#
+# Plot spherical height data
+#
+fig = plt.figure(figsize=[10, 10])
+ax = fig.add_subplot(projection='3d')
+
+wxs, wys = getOrthHemisphereXYCoords(westHemiSmall)
+exs, eys =getOrthHemisphereXYCoords(eastHemiSmall)
+
+wcs = getHemispherePixels(westHemiSmall)
+ecs = getHemispherePixels(eastHemiSmall)
+
+r, wlmbdas, wphis = inverseOrthographic(wxs, wys, max(hemisphereDims)//sf / 2, lmbda0=-90)
+_, elmbdas, ephis = inverseOrthographic(exs, eys, max(hemisphereDims)//sf / 2, lmbda0=90)
+
+lmbdas = np.concatenate((wlmbdas, elmbdas))
+phis = np.concatenate((wphis, ephis))
+
+csBgr = np.concatenate((wcs, ecs))
+csHsv = cv2.cvtColor(np.array([csBgr]), cv2.COLOR_BGR2HSV)[0]
+
+mappedCsHsvIdx = findNearestColorIdx(csHsv, cmapHsv, metric=ColorMetric.L2NORM, weights=[4, 1, 2])
+# mappedCsBgrIdx = findNearestColorIdx(csBgr, cmapBgr)
+
+#
+# East hemisphere altitudes
+#
+# mappedWcsHsvIdx = findNearestColorIdx(cv2.cvtColor(np.array([ecs]), cv2.COLOR_BGR2HSV)[0], cmapHsv)
+# mappedWcsHsv = cmapHsv[mappedWcsHsvIdx]
+# wAltitudes = getHeightFromCmapIdx(mappedWcsHsvIdx, cmapHsv, heightRange)
+# wAltPixels = (wAltitudes-heightRange.min())/((heightRange.max()-heightRange.min())/255.0)
+# wAltPixels = (np.around(wAltPixels)).astype(img.dtype)
+
+# nonzeroCounts = np.count_nonzero(np.count_nonzero(westHemiSmall, axis=-1), axis=-1)
+# splitIndices = np.cumsum(nonzeroCounts) - 5
+# wAltImg = []
+# for row in np.split(wAltPixels, splitIndices):
+#     newRowLen = westHemiSmall.shape[1]
+#     spaceToFill = newRowLen - len(row)
+#     if (spaceToFill) % 2 == 0:
+#         pad = spaceToFill // 2
+#     else:
+#         pad = ((spaceToFill // 2 + 1), spaceToFill // 2)
+
+#     wAltImg.append(np.pad(row, pad, 'constant'))
+
+# wAltImg = np.array(wAltImg)
+
+# diffs = np.abs(mappedCsBgrIdx-mappedCsHsvIdx)
+
+# mappedCsBgr = cmapBgr[mappedCsBgrIdx]
+mappedCsHsv = cmapHsv[mappedCsHsvIdx]
+
+# altitudesBgr = getHeightFromCmapIdx(mappedCsBgrIdx, cmapBgr, heightRange)
+altitudesHsv = getHeightFromCmapIdx(mappedCsHsvIdx, cmapHsv, heightRange)
+# heightMapBgr = r + 0.0005 * altitudesBgr
+heightMapHsv = r + 0.0005 * altitudesHsv
+
+xs, ys, zs = geoToCartesian(heightMapHsv, lmbdas, phis)
+
+# ax.scatter(xs, ys, zs, s=1, c=np.flip(mappedCsBgr, axis=-1)/255.0)
+ax.scatter(xs, ys, zs, s=1, c=cv2.cvtColor(np.array([mappedCsHsv]), cv2.COLOR_HSV2RGB)[0]/255.0)
+ax.set_box_aspect((2, 2, 2))
+ax.set(xlabel='x', ylabel='y', zlabel='z')
+plt.show()
+
+
+#
+# Plot spherical original pixel data
+#
+# fig = plt.figure(figsize=[10, 10])
+# ax = fig.add_subplot(projection='3d')
+
+# xs, ys, zs = geoToCartesian(r, lmbdas, phis)
+
+# ax.scatter(xs, ys, zs, s=1, c=np.flip(cs, axis=-1)/255.0)
+# ax.set_box_aspect((2, 2, 2))
+# ax.set(xlabel='x', ylabel='y', zlabel='z')
+# plt.show()
+
+#
+# Show some image
+#
+# cv2.imshow('img', np.tile(cmapBgr, (20, 1, 1)))
+# cv2.imshow('img2', cv2.cvtColor(np.tile(cmapHsv, (20, 1, 1)), cv2.COLOR_HSV2BGR))
+# # cv2.imshow('img', westHemiSmall)
+# # # cv2.imwrite('wAlt.png', np.array((wAltImg)))
+# cv2.imshow('img2', np.array((wAltImg)))
+
+# wait_time = 1000
+# while cv2.getWindowProperty('img', cv2.WND_PROP_VISIBLE) >= 1 or \
+#       cv2.getWindowProperty('img2', cv2.WND_PROP_VISIBLE):
+#     keyCode = cv2.waitKey(wait_time)
+#     if (keyCode & 0xFF) == ord("q"):
+#         cv2.destroyAllWindows()
+#         break
---
+++ utils.py
@@ -0,0 +1,193 @@
+import numpy as np
+from enum import Enum, auto
+
+class ColorMetric(Enum):
+    L2NORM = auto()
+    L1NORM = auto()
+    MSE = auto()
+    MAE = auto()
+
+def orthographic(r, lmbda, phi, lmbda0=0, phi0=0):
+    '''
+    Compute orthographic project of spherical coords. to cartesian (x, y).
+
+    r     = radius,
+    lmbda = longitude in degrees (vertical lines),
+    phi   = latitude in degrees (horizontal lines)
+
+    (lbmda0, phi0) = centre of projection
+    '''
+    l, l0 = np.radians(lmbda), np.radians(lmbda0)
+    p, p0 = np.radians(phi), np.radians(phi0)
+
+    x = r * np.cos(p) * np.sin(l-l0)
+    y = r * (np.cos(p0)*np.sin(p) - np.sin(p0)*np.cos(p)*np.cos(l-l0))
+
+    return x, y
+
+
+def inverseOrthographic(x, y, r, lmbda0=0, phi0=0):
+    '''
+    Compute inverse orthographic projection from (x, y) to long/lat coords.
+    '''
+    l0, p0 = np.radians(lmbda0), np.radians(phi0)
+
+    rho = np.sqrt(x**2 + y**2)
+    rhoOverR = rho/r
+    rhoOverR[rhoOverR > 1] = 1
+    rhoOverR[rhoOverR < -1] = -1
+    c = np.arcsin(rhoOverR)
+
+    sc, cc = np.sin(c), np.cos(c)
+    sp0, cp0 = np.sin(p0), np.cos(p0)
+
+    tempRho = np.copy(rho)
+    tempRho[rho == 0] = 1
+    p = np.arcsin(cc*sp0 + y*sc*cp0/tempRho)
+    l = l0 + np.arctan2(x*sc, rho*cc*cp0 - y*sc*sp0)
+
+    lmbda, phi = np.degrees(l), np.degrees(p)
+
+    return r, lmbda, phi
+
+
+def geoToCartesian(r, lmbda, phi):
+    '''
+    Convert geographical coordinates into equivalent 3D Cartesian coords.
+    '''
+    theta, p = np.radians(lmbda), np.radians(90-phi)
+
+    x = r * np.sin(p) * np.cos(theta)
+    y = r * np.sin(p) * np.sin(theta)
+    z = r * np.cos(p)
+
+    return x, y, z
+
+
+def stableUnique(arr: np.ndarray, axis: int):
+    u, idx = np.unique(arr, axis=axis, return_index=True)
+    return arr[np.sort(idx)]
+
+
+def interpColormap(colormap: np.ndarray, pointsPerSample: int, isHsv=False):
+    if isHsv:
+        limits = [180, 256, 256]
+    else:
+        limits = 256
+
+    N = len(colormap)
+    signedCmap = colormap.astype(int)
+    newSize = N + (N-1) * pointsPerSample
+    interp = np.zeros((newSize, 3))
+
+    interp[np.arange(N)*(pointsPerSample+1)] = signedCmap
+    starts, ends = np.arange(N-1), np.arange(1, N)
+
+    for i in range(1, pointsPerSample+1):
+        diffs = signedCmap[ends] - signedCmap[starts]
+
+        if isHsv:
+            shortcuts = diffs[:, 0][np.abs(diffs[:, 0]) > 90]
+            if shortcuts.size > 0:
+                shortcuts = (-np.abs(shortcuts)) % 180
+                diffs[:, 0][np.abs(diffs[:, 0]) > 90] = shortcuts
+
+        idx = starts * (pointsPerSample+1) + i
+        interp[idx] = (signedCmap[starts] + i * (diffs/(pointsPerSample+1))) % limits
+
+    interp = np.around(interp).astype(colormap.dtype)
+    interp = stableUnique(interp, axis=0)
+
+    return interp
+
+
+def getBoxRegion(img: np.ndarray, topleft: np.ndarray, dims: np.ndarray):
+    return img[
+        topleft[1]:(topleft+dims)[1],
+        topleft[0]:(topleft+dims)[0]
+    ]
+
+
+def getOrthHemisphereXYCoords(hemisphere: np.ndarray):
+    indices = np.where(np.all(np.greater(hemisphere, np.zeros(3)), axis=-1))
+    diam = np.ceil(max(hemisphere.shape) / 2)
+    return indices[1] - diam, diam - indices[0]
+
+
+def getHemispherePixels(hemisphere: np.ndarray):
+    return hemisphere[np.where(np.all(np.greater(hemisphere, np.zeros(3)), axis=-1))]
+
+
+def findNearestColorIdx(colors: np.ndarray, colormap: np.ndarray,
+                        metric=ColorMetric.L1NORM, weights=[1, 1, 1]):
+    if metric == ColorMetric.L2NORM:
+        idx = np.linalg.norm(
+            (colormap[np.newaxis,:,:] - colors[:,np.newaxis,:]) * weights,
+            2,
+            axis=-1
+        ).argmin(axis=-1)
+
+    elif metric == ColorMetric.L1NORM:
+        idx = np.linalg.norm(
+            (colormap[np.newaxis,:,:] - colors[:,np.newaxis,:]) * weights,
+            1,
+            axis=-1
+        ).argmin(axis=-1)
+
+    elif metric == ColorMetric.MSE:
+        idx = np.mean(
+            ((colormap[np.newaxis,:,:] - colors[:,np.newaxis,:]) * weights)**2,
+            axis=-1
+        ).argmin(axis=-1)
+
+    elif metric == ColorMetric.MAE:
+        idx = np.mean(
+            np.abs((colormap[np.newaxis,:,:] - colors[:,np.newaxis,:]) * weights),
+            axis=-1
+        ).argmin(axis=-1)
+
+    else:
+        raise LookupError('Invalid metric given')
+
+    return idx
+
+
+def getHeightFromCmapIdx(idx, colormap, heightRange):
+    start, stop = heightRange
+    return np.linspace(start, stop, len(colormap))[idx]
+
+
+
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+    import matplotlib.gridspec as gridspec
+
+    r = 90
+    lmbdas = phis = np.arange(-90, 100, 10)
+
+    lv, pv = np.meshgrid(lmbdas, phis)
+    xs, ys = orthographic(r, lv, pv)
+
+    fig = plt.figure(figsize=[8, 12])
+    gs = gridspec.GridSpec(nrows=2, ncols=2, width_ratios=[1, 1], height_ratios=[1, 2])
+
+    ax1 = fig.add_subplot(gs[0, 0])
+    ax1.scatter(lv, pv, c=np.arange(len(lmbdas)**2), cmap='plasma')
+    ax1.set_title('(longitude, latitude) values')
+    ax1.set(xlabel='longitude (degrees)', ylabel='latitude (degrees)')
+
+    ax2 = fig.add_subplot(gs[0, 1])
+    ax2.scatter(xs, ys, c=np.arange(len(lmbdas)**2), cmap='plasma')
+    ax2.set_title('projected (x, y) values')
+    ax2.set(xlabel='x', ylabel='y')
+
+    rs, lmbdas, phis = inverseOrthographic(xs, ys, r)
+    xs, ys, zs = geoToCartesian(rs, lmbdas, phis)
+    #xs, ys, zs = geoToCartesian(rs, lv, pv)
+
+    ax3 = fig.add_subplot(gs[1, :], projection='3d')
+    ax3.scatter(xs, ys, zs, c=np.arange(len(lmbdas)**2), cmap='plasma')
+    ax3.set_box_aspect((1, 2, 2))
+    ax3.set_title('projected (x, y) -> inverse projected (r, long., lat.) -> converted (x, y, z)', y=-0.2)
+    ax3.set(xlabel='x', ylabel='y', zlabel='z')
+    plt.show()
